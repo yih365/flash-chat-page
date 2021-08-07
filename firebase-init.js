c9a0f76e6ca695f8569db40046c7c69b6e1ca7c7
yih365 <68409635+yih365@users.noreply.github.com>
2021-08-06 20:40:55 -0700

    Update firebase-init.js

--- firebase-init.js
+++ firebase-init.js
@@ -2,14 +2,14 @@
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBkM2wLnBGNhJU51NsIsj1tXYcbb5iZGtE",
-  authDomain: "new-project-24a62.firebaseapp.com",
-  projectId: "new-project-24a62",
-  storageBucket: "new-project-24a62.appspot.com",
-  messagingSenderId: "387132142073",
-  appId: "1:387132142073:web:0a7075b2cd1457d6508d28"
+  apiKey: "your-apiKey",
+  authDomain: "your-authDomain.firebaseapp.com",
+  projectId: "your-projectId",
+  storageBucket: "your-storage-bucket.appspot.com",
+  messagingSenderId: "your-messagingSenderId",
+  appId: "your-appID"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
-const TEXT_COLLECTION = "texts";
+const TEXT_COLLECTION = "texts"; // name of the firestore collection
