7b72216ef59a23bb53fd3354481c9dcb49c578ba
Glitch (text-boxes) <none>
2021-08-07 03:54:39 +0000

    Updated with Glitch

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# Flash Chat Web Page
+# Flash Chat Web App
+
+This is a web app that allows users to type into the chat page. Different users get different colors for their messages. Messages appear in a random location on the canvas. Each message also disappears after 5 second.
+
+`firebase-init.js` contains the integration information for firebase firestore that includes a collection named "texts". 
 
--- firebase-init.js
+++ firebase-init.js
@@ -2,14 +2,14 @@
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "your-apiKey",
-  authDomain: "your-authDomain.firebaseapp.com",
-  projectId: "your-projectId",
-  storageBucket: "your-storage-bucket.appspot.com",
-  messagingSenderId: "your-messagingSenderId",
-  appId: "your-appID"
+  apiKey: "AIzaSyBkM2wLnBGNhJU51NsIsj1tXYcbb5iZGtE",
+  authDomain: "new-project-24a62.firebaseapp.com",
+  projectId: "new-project-24a62",
+  storageBucket: "new-project-24a62.appspot.com",
+  messagingSenderId: "387132142073",
+  appId: "1:387132142073:web:0a7075b2cd1457d6508d28"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
-const TEXT_COLLECTION = "texts"; // name of the firestore collection
+const TEXT_COLLECTION = "texts";
