2555c317676e7c7b7253b4a306fa711757325657
yih365 <68409635+yih365@users.noreply.github.com>
2021-08-06 20:45:48 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 // Be sure to name any p5.js functions/variables we use in the global so Glitch can recognize them.
 // Add to this list as you consult the p5.js documentation for other functions.
 /* global circle, createVector, square, angleMode, DEGREES, arc, clear, createCanvas, colorMode, HSB, width, height, random, background, fill, color, random,
-          rect, ellipse, stroke, image, loadImage, collideCircleCircle, collideRectCircle, text, 
+          rect, ellipse, stroke, image, loadImage, text, 
           mouseX, sq, mouseY, strokeWeight, line, mouseIsPressed, noFill, windowWidth, windowHeight, noStroke, 
-          keyCode, TEXT_COLLECTION, firebase, LEFT, createButton, textAlign, CENTER, createInput, collidePointCircle, round, sqrt, lerpColor, PI, HALF_PI, UP_ARROW, LEFT_ARROW, RIGHT_ARROW, DOWN_ARROW, textSize */
+          keyCode, TEXT_COLLECTION, firebase, LEFT, createButton, textAlign, CENTER, createInput, round, sqrt, lerpColor, PI, HALF_PI, UP_ARROW, LEFT_ARROW, RIGHT_ARROW, DOWN_ARROW, textSize */
 
 let input;
 let button;
